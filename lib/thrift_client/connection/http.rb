ab27b71883485abdcef6ab8955d902f7e7f7e3fc
Brandon Mitchell <brandon@twitter.com>
2012-09-10 13:36:58 -0700

    Push Connection into ThriftHelpers to prevent namespace pollution.

--- lib/thrift_client/connection/http.rb
+++ lib/thrift_client/connection/http.rb
@@ -1,18 +1,20 @@
-module Connection
-  class HTTP < Base
-    def connect!
-      uri = parse_server(@server)
-      @transport = Thrift::HTTPClientTransport.new(@server)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = uri.scheme == "https"
-      http.get(uri.path)
-    end
+module ThriftHelpers
+  module Connection
+    class HTTP < Base
+      def connect!
+        uri = parse_server(@server)
+        @transport = Thrift::HTTPClientTransport.new(@server)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == "https"
+        http.get(uri.path)
+      end
 
-  private
-    def parse_server(server)
-      uri = URI.parse(server)
-      raise ArgumentError, 'Servers must start with http' unless uri.scheme =~ /^http/
-      uri
+      private
+      def parse_server(server)
+        uri = URI.parse(server)
+        raise ArgumentError, 'Servers must start with http' unless uri.scheme =~ /^http/
+        uri
+      end
     end
   end
 end
